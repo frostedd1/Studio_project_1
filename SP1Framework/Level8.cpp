6168184b6461cd58c629ce23460acbe34557c4f6
Keldop <42366824+Keldop@users.noreply.github.com>
2018-08-15 10:20:40 +0800

    map9

---
+++ SP1Framework/Level8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+string firstlevel()
+{
+	string line;
+	ifstream myfile("map9.txt");
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			for (int index = 0; index < line.size(); index++)
+			{
+				switch (line[index])
+				{
+				case '#':
+					line[index] = 219;
+					break;
+				case '$':
+					line[index] = 176;
+					break;
+				case '@':
+					line[index] = 'M';
+					break;
+				case '%':
+					line[index] = 'E';
+					break;
+				}
+			}
+
+		}
+		myfile.close();
+	}
+
+
+	return line;
+}
