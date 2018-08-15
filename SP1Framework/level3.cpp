490648f751cdc13f7b277931ea8b65c359be949b
ChikatoXXVII <ChikatoXXVII@users.noreply.github.com>
2018-08-15 16:27:44 +0800

    new stuff

--- SP1Framework/level3.cpp
+++ SP1Framework/level3.cpp
@@ -12,7 +12,7 @@ string thirdlevel()
 	{
 		while (getline(myfile, line))
 		{
-			for (int index = 0; index < line.size(); index++)
+			for (unsigned int index = 0; index < line.size(); index++)
 			{
 				switch (line[index])
 				{
